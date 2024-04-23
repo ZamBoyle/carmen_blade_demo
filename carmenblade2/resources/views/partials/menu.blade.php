fab34caa0d274bde7168c4738a65375687e113d6
Johnny Piette <johnny.piette@gmail.com>
2024-04-24 01:18:43 +0200

    Updates

--- carmenblade2/resources/views/partials/menu.blade.php
+++ carmenblade2/resources/views/partials/menu.blade.php
@@ -39,30 +39,3 @@
         </div>
     </div>
 </nav>
-
-
-
-{{-- <nav class="navbar navbar-expand-lg bg-primary mb-2" data-bs-theme="dark">
-    <a class="visually-hidden-focusable" href="#content">Aller au contenu</a>
-    <div class="container-fluid">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Accueil</a>
-                </li>
-                <li class="nav-item"><a class="nav-link" href="{{ route('history') }}">Notre histoire</a></li>
-                <li class="nav-item"><a class="nav-link" href="{{ route('goals') }}">Nos objectifs</a></li>
-                <li class="nav-item"><a class="nav-link" href="support">Comment nous
-                        soutenir ?</a></li>
-                <li class="nav-item"><a class="nav-link"
-                        href="organigram">Organigramme de l'association</a></li>
-                <li class="nav-item"><a class="nav-link" href="contact">Contact</a></li>
-                <li class="nav-item"><a class="nav-link" href="galery">Gallerie photo</a></li>
-            </ul>
-        </div>
-    </div>
-</nav> --}}
